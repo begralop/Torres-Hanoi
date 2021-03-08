f586f4ff2d35a4b5d15913d67adb3fdee5ba4042
belen <bgralop@epsg.upv.es>
2021-03-08 19:15:56 +0100

    Añadido constructor a la claase

--- Torres_de_Hanoi/Disco.cs
+++ Torres_de_Hanoi/Disco.cs
@@ -10,5 +10,9 @@ namespace Torres_de_Hanoi
     {
         public int valor { get; set; } // Definimos parámetro valor (he elegido representarlo con enteros) con get y set
 
+        public Disco(int num) // Constructor 
+        {
+            this.valor = num; // Asignamos el valor num del constructor al parámetro valor
+        }
     }
 }
