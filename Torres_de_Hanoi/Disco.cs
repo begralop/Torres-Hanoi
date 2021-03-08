c95e87f577859c142be169178d055c4fed584f1a
belen <bgralop@epsg.upv.es>
2021-03-08 18:36:57 +0100

    Decidido usar valores enteros en la clase disco

--- Torres_de_Hanoi/Disco.cs
+++ Torres_de_Hanoi/Disco.cs
@@ -8,11 +8,7 @@ namespace Torres_de_Hanoi
 {
     class Disco
     {
-        /*TODO: 
-        Decidir tipo de Valor
-        public int Valor { get; set; }
-        public String Valor { get; set; }
-        */
+        public int valor { get; set; } // Definimos parámetro valor (he elegido representarlo con enteros) con get y set
 
     }
 }
