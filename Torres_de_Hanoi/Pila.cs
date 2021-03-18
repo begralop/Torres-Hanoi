161de788d4bdf022a358d00a52d6303c7471b7c7
belen <bgralop@epsg.upv.es>
2021-03-18 12:37:02 +0100

    Clase pila con los métodos push, pop y isEmpty

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -23,5 +23,43 @@ namespace Torres_de_Hanoi
             Top = 100; // Asignamos 100 al valor del disco superior para pilas vacías
         }
 
+        public void push(Disco d) // Permite colocar un disco en la pila de discos
+        { 
+            Elementos[Size] = d; // Añadimos a la pila el disco que le pasamos
+            Size = Size + 1; // El tamaño de la pila será igual al tamaño más uno (porque hemos añadido 1)
+            Top = d.valor; // Asignamos a top el valor del disco colocado
+        }
+
+        public Disco pop() // Permite extraer el disco de la parte superior de la pila.
+        {
+            if (isEmpty() == true) // Si el metod está vació vale true
+            {
+                return null; // Devuelve null
+            }
+            else // Si no
+            {
+                Disco aux = Elementos[Size - 1]; // Asignamos a un disco aux el que está en la parte superior de la pila
+                Size = Size - 1; // El tamaño será el tamaño menos uno (por el que vamos a extraer)
+                if (isEmpty() == true) // Si el metodo devuelve true
+                {
+                    Top = 100; // El valor del disco superior será 100
+                }
+                else // Si no
+                {
+                    Top = Elementos[Size - 1].valor; // Top valdrá el valor del disco superior de la pila
+                }
+                return aux; // Devuelve el disco aux
+            }
+        }
+
+        public bool isEmpty() // Informa si la pila está vacía o no
+        {
+            if (Size == 0) // Si el tamaño es 0
+            {
+                return true; // Devuelve true
+            }
+            return false; // Si no devuelve false
+        }
+
     }
 }
