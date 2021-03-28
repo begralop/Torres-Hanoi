8ad29e58a0c7fa7b565126396c50397f3487af17
belen <bgralop@epsg.upv.es>
2021-03-28 14:22:52 +0200

    Cambiados comentarios para mayor comprensión y añadido el writeLine cuando es par para que muestre en cuantos movimientos se hizo

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -11,15 +11,15 @@ namespace Torres_de_Hanoi
 
         int movimientos = 0; // Variable global movimientos inicializada en 0 que almacena la cantidad de movimientos que se realiza
 
-        public void mover_disco(Pila a, Pila b) // Método mover_disco para comprobar de que pila a pila hacer el movimiento
+        public void mover_disco(Pila a, Pila b) // Método mover_disco que se le pasan dos pilas que es a las que se va a hacer el movimiento
         {
-            if (a.Top < b.Top) // Si la pila a es menor que la b el movimiento se hace de b a a
+            if (a.Top < b.Top) // Si el disco superior de la pila a es menor que el de la b el movimiento se hace de a a b
             {
                 Disco d = a.pop();
                 b.push(d); // Aquí se hace el movimiento con los métodos push y pop de la clase pila
                 Console.WriteLine("El disco " + d.valor + " se mueve de la " + a.nombre + " a la " + b.nombre);
             }
-            else // En caso de que b sea menor que a, el movimiento se hace de la pila a a la pila b
+            else // En caso de que el disco superior de b sea menor que el de a, el movimiento se hace de la pila a b la pila a
             {
                 Disco d = b.pop();
                 a.push(d); // Aquí se hace el movimiento con los métodos push y pop de la clase pila
@@ -33,45 +33,46 @@ namespace Torres_de_Hanoi
             {
                 while (fin.Size != n) // Hacemos los siguientes movimientos hasta que la pila fin tenga el nº de discos iniciales
                 {
-                    mover_disco(ini, aux); // El primer movimiento es de la pila ini a la aux
+                    mover_disco(ini, aux); // El primer movimiento es de la pila ini a la aux, llamamos al metodo pasandole esas pilas
                     movimientos = movimientos + 1; // Los movimientos valdrán este más uno
-                    if (fin.Size == n) // Si el contador llega al cálculo de movimientos mínimos
+                    if (fin.Size == n) // Si el tamaño de la pila fin es el mismo que el número de discos que se le ha pasado
                     {
                         break; // Hacemos que el método pare
                     }
-                    mover_disco(ini, fin); // El segundo movimiento es de la pila ini a la fin
+                    mover_disco(ini, fin); // El segundo movimiento es de la pila ini a la fin, llamamos al metodo pasandole esas pilas
                     movimientos = movimientos + 1; // Los movimientos valdrán este más uno
-                    if (fin.Size == n) // Si el contador llega al cálculo de movimientos mínimos
+                    if (fin.Size == n) // Si el tamaño de la pila fin es el mismo que el número de discos que se le ha pasado
                     {
                         break; // Hacemos que el método pare
                     }
-                    mover_disco(fin, aux); // El tercer movimiento es de la pila fin a la aux
+                    mover_disco(fin, aux); // El tercer movimiento es de la pila fin a la aux, llamamos al metodo pasandole esas pilas
                     movimientos = movimientos + 1; // Los movimientos valdrán este más uno
-                    if (fin.Size == n) // Si el contador llega al cálculo de movimientos mínimos
+                    if (fin.Size == n) // Si el tamaño de la pila fin es el mismo que el número de discos que se le ha pasado
                     {
                         break; // Hacemos que el método pare
                     }
                 }
+                Console.WriteLine("Se hizo en: " + movimientos + " movimientos"); // Muestra por pantalla en cuantos movimientos se hizo
             }
-            else // Si no
+            else // Si no es par
             {
                 while (fin.Size != n) // Hacemos los siguientes movimientos hasta que la pila fin tenga el nº de discos iniciales
                 {
-                    mover_disco(ini, fin); // El primer movimiento es de la pila ini a la fin
+                    mover_disco(ini, fin); // El primer movimiento es de la pila ini a la fin, llamamos al metodo pasandole esas pilas
                     movimientos = movimientos + 1; // Los movimientos valdrán este más uno
-                    if (fin.Size == n) // Si el contador llega al cálculo de movimientos mínimos
+                    if (fin.Size == n) // Si el tamaño de la pila fin es el mismo que el número de discos que se le ha pasado
                     {
                         break;
                     }
-                    mover_disco(ini, aux); // El segundo movimiento es de la pila ini a la aux
+                    mover_disco(ini, aux); // El segundo movimiento es de la pila ini a la aux, llamamos al metodo pasandole esas pilas
                     movimientos = movimientos + 1; // Los movimientos valdrán este más uno
-                    if (fin.Size == n) // Si el contador llega al cálculo de movimientos mínimos
+                    if (fin.Size == n) // Si el tamaño de la pila fin es el mismo que el número de discos que se le ha pasado
                     {
                         break; // Hacemos que el método pare
                     }
-                    mover_disco(aux, fin); // El tercer movimiento es de la pila aux a la fin
+                    mover_disco(aux, fin); // El tercer movimiento es de la pila aux a la fin, llamamos al metodo pasandole esas pilas
                     movimientos = movimientos + 1; // Los movimientos valdrán este más uno
-                    if (fin.Size == n) // Si el contador llega al cálculo de movimientos mínimos
+                    if (fin.Size == n) // Si el tamaño de la pila fin es el mismo que el número de discos que se le ha pasado
                     {
                         break; // Hacemos que el método pare
                     }
